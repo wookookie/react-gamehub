dbc20e84ce861db7c706d9f8049381021782c052
wookookie <wookookie404@gmail.com>
2024-03-18 20:18:03 +0900

    Fix that unique key prop warning

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -27,7 +27,7 @@ export function PlatformIconList({ platforms }: Props) {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
