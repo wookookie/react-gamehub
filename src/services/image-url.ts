435785b17045050032bf93d1c6931877e8338e4d
wookookie <wookookie404@gmail.com>
2024-03-19 13:59:10 +0900

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,11 +1,13 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 /**
  * 크기를 줄인 이미지를 요청하는 URL을 반환합니다.
  * @param url
  * @returns
  */
 export function getCroppedImageUrl(url: string) {
-  // 이미지가 없는 게임일 경우 비어있는 문자열 반환
-  if (!url) return "";
+  // 이미지가 없는 게임일 경우 기본 이미지 반환
+  if (!url) return noImage;
 
   // 기존: https://media.rawg.io/media/games/...
   // 반환: https://media.rawg.io/media/crop/600/400/games/...
