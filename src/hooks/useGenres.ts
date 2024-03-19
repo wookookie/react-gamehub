f3372ea46be2807ec080ceea6b231f977390c174
wookookie <wookookie404@gmail.com>
2024-03-19 15:42:34 +0900

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import { useData } from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -8,5 +8,7 @@ export interface Genre {
 
 // Hiding details behind the 'useGenres' hook
 export function useGenres() {
-  return useData<Genre>("/genres");
+  // Static data 사용
+  // 사용 중인 컴포넌트가 영향을 받지 않도록 하기 위해 hook과 동일한 항목 반환
+  return { data: genres, error: null, isLoading: false };
 }
