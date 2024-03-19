ee81397e7ac85a81aafe0923ae6a33068a7ba96c
wookookie <wookookie404@gmail.com>
2024-03-19 14:15:04 +0900

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -3,10 +3,11 @@ import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 export function ColorModeSwitch() {
   const { colorMode, toggleColorMode } = useColorMode();
 
+  // whiteSpace="nowrap" -> 단어의 공백에 대한 줄 바꿈 방지
   return (
     <HStack>
       <Switch colorScheme="green" isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 }
